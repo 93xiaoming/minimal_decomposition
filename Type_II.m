2e78c02453f23e5b80bed2a667f756411bc3b5f3
93xiaoming <39090764+93xiaoming@users.noreply.github.com>
2019-03-05 09:53:32 +0800

    Update Type_II.m

--- Type_II.m
+++ Type_II.m
@@ -28,6 +28,8 @@ function output = Type_II( theta,psi,phi,Theta )
     if pmin==1  %pmin==1?
         output = [theta;phi];
         
+    elseif pmin==2
+        output = p2( theta,psi,phi,Theta );
     elseif pmin==p_odd     %pmin is odd?
         
         output = decomp_odd(theta,psi,phi,Theta,pmin,num_odd);
@@ -163,23 +165,36 @@ end
 function Rot = Rot(theta,psi,phi)   
 sx=[0 1;1 0];sy=[0 -i;i 0];sz=[1 0;0 -1];
 Rot=expm(-i*(sin(theta)*cos(psi)*sx+sin(theta)*sin(psi)*sy+cos(theta)*sz)*phi/2);
-
-psi22=angle(Rot(2,1)/Rot(1,2));  
-if psi22<0
-    psi22=psi22+2*pi;
 end
-psi2=psi22/2;
 
-    phi2=2*acos(real(Rot(1,1)));
-    if imag(Rot(1,1))>0
-        phi2=4*pi-phi2;
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%Output: decomposition for p=2
+function output = p2( theta,psi,phi,Theta )
+    a=( sin(psi)*cos(phi/2)+cos(psi)*sin(phi/2)*cos(theta))/(sin(phi/2)*sin(theta));
+    b=(-sin(psi)*cos(phi/2)+cos(psi)*sin(phi/2)*cos(theta))/(sin(phi/2)*sin(theta));
+
+  
+    if a>=cot(Theta);
+        bol=1;
+        
+        phi1=2*pi+ (2*acos( cos(phi/2)*cos(psi)-sin(phi/2)*sin(psi)*cos(theta))-2*pi)*sign(sin(phi/2));
+        phi2=mod(-2*psi,4*pi);
+        
+        output = [acot(a)+pi*(acot(a)<0), 0;phi1, phi2];
+    elseif b>=cot(Theta);
+        bol=1;
+        
+        phi1=2*psi;
+        phi2=2*pi+ (2*acos( cos(phi/2)*cos(psi)+sin(phi/2)*sin(psi)*cos(theta))-2*pi)*sign(sin(phi/2));
+                     
+        output = [0, acot(b)+pi*(acot(b)<0);phi1, phi2];
     end
-    theta2= atan( abs( Rot(2,1)/imag(Rot(1,1)) ) );
+end  
 
 
-Rot(2,1)*i;
-sin(phi/2)*sin(theta)*exp(i*psi2);
 
-end
+
+
 
 
